80de47d86697dbfa8eec8eb408ac079320a86c7b
Mehradi <mehradi.info@gmail.com>
2023-08-15 22:10:31 +0200

    Adding Test: functions should to be strictly equal

--- src/examples/great-expectations/objects.test.ts
+++ src/examples/great-expectations/objects.test.ts
@@ -1,11 +1,14 @@
 import { expect, test, describe } from "vitest";
 
 describe("toBe", () => {
-  test("objects should not be strictly equal", () => {
-    expect({ a: 1 }).not.toBe({ a: 1 });
+  test.fails("objects should not be strictly equal", () => {
+    expect({ a: 1 }).toBe({ a: 1 });
   });
 
   test.fails("arrays should be strictly equal", () => {
     expect([1, 2, 3]).toBe([1, 2, 3]);
+  });
+  test.fails("functions should to be strictly equal", () => {
+    expect(() => {}).toBe(() => {});
   });
 });
