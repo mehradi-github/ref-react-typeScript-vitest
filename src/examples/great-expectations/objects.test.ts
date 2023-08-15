3d7f8ee49d65e8e06eadb80c209b000ffd5d8c35
Mehradi <mehradi.info@gmail.com>
2023-08-15 22:04:20 +0200

    Adding Test: objects should not be strictly equal

--- src/examples/great-expectations/objects.test.ts
+++ src/examples/great-expectations/objects.test.ts
@@ -1,7 +1,7 @@
 import { expect, test, describe } from "vitest";
 
 describe("toBe", () => {
-  test.fails("objects should not be strictly equal", () => {
-    expect({ a: 1 }).toBe({ a: 1 });
+  test("objects should not be strictly equal", () => {
+    expect({ a: 1 }).not.toBe({ a: 1 });
   });
 });
