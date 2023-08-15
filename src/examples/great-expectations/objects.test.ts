f5b93b6042971794538585a97e6883ac7249934a
Mehradi <mehradi.info@gmail.com>
2023-08-15 22:08:57 +0200

    Adding Test: arrays should be strictly equal

--- src/examples/great-expectations/objects.test.ts
+++ src/examples/great-expectations/objects.test.ts
@@ -4,4 +4,8 @@ describe("toBe", () => {
   test("objects should not be strictly equal", () => {
     expect({ a: 1 }).not.toBe({ a: 1 });
   });
+
+  test.fails("arrays should be strictly equal", () => {
+    expect([1, 2, 3]).toBe([1, 2, 3]);
+  });
 });
