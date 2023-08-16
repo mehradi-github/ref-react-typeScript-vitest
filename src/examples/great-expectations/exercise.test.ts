73275a43f10c1059b77e0c55a3462ff5c419c6d7
Mehradi <mehradi.info@gmail.com>
2023-08-16 09:56:36 +0200

    Adding Test:should include Backlog in board.statuses

--- src/examples/great-expectations/exercise.test.ts
+++ src/examples/great-expectations/exercise.test.ts
@@ -1,5 +1,14 @@
-import { expect, it } from "vitest";
+import { describe, expect, it } from "vitest";
+import { KanbanBoard } from "$lib/kanban-board";
 
 it("should pass if the two numbers would add up correctly in a language other than JavaScript", () => {
   expect(0.2 + 0.1).toBeCloseTo(0.3);
+});
+describe("Kanban Board", () => {
+  it('should include "Backlog" in board.statuses', () => {
+    const board = new KanbanBoard("Things to Do");
+    expect.hasAssertions();
+    // Verify that board.statuses contains "Backlog".
+    expect(board.statuses).toContain("Backlog");
+  });
 });
