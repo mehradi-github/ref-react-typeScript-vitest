6fb10bd3162b71724f0add538d49aee8ce423ffd
Mehradi <mehradi.info@gmail.com>
2023-08-23 08:45:23 +0200

    Checking toMatchInlineSnapshot

--- src/examples/logjam/log.test.ts
+++ src/examples/logjam/log.test.ts
@@ -1,7 +1,11 @@
 import { test, expect, vi } from 'vitest';
 
 test('it spies on the multiply method', () => {
-  const mock = vi.fn();
-  mock();
+  const mock = vi.fn((x?: string) => {
+    if (x) return x.repeat(3);
+  });
+  const result = mock('wow');
   expect(mock).toHaveBeenCalled();
+  expect(mock).toHaveBeenCalledWith('wow');
+  expect(result).toMatchInlineSnapshot('"wowwowwow"');
 });
